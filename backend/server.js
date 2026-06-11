df7cc85bf2e64abc08c561a2234ec682d98be97e
rani-4524 <raniagalave@gmail.com>
2026-06-11 20:27:32 +0530

    Fixed some issues in backend

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,7 @@ app.post("/api/send-mail", async (req, res) => {
     }
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
